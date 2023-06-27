a78902e6dc4cb5006e03e2c563e9868907f75278
Kamil Holody <kamilholody@live.com>
2023-06-27 16:15:52 +0200

    243 - Set City name

---
+++ src/test/java/HotelSearch.java
@@ -0,0 +1,30 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class HotelSearch {
+
+    @Test
+    public void searchHotel() throws InterruptedException {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10L, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get("http://www.kurs-selenium.pl/demo/");
+        // Line of code created by Me
+//        WebElement searchField = driver.findElement(By.cssSelector(".select2-choice"));
+//        searchField.click();
+//        searchField.sendKeys("Dubai");
+        // Line of code creted by udemy
+        driver.findElement(By.xpath("//span[text()='Search by Hotel or City Name']")).click();
+        driver.findElement(By.xpath("//div[@id='select2-drop']//input")).sendKeys("Dubai");
+        driver.findElement(By.xpath("//span[@class='select2-match' and text()='Dubai']")).click();
+
+    }
+}
