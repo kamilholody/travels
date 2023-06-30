fa19d4efa75d2332b924b8fcffa4c7490c1aa12c
Kamil Holody <kamilholody@live.com>
2023-06-30 13:36:10 +0200

    251 - Completing the registration form II

--- src/test/java/SignUpTest.java
+++ src/test/java/SignUpTest.java
@@ -3,6 +3,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import java.util.concurrent.TimeUnit;
 
@@ -18,6 +19,7 @@ public class SignUpTest {
         driver.manage().window().maximize();
         driver.get("http://www.kurs-selenium.pl/demo/");
 
+        String lastName = "Testowy";
         driver.findElements(By.xpath("//li[@id='li_myaccount']"))
                 .stream()
                 .filter(WebElement::isDisplayed)
@@ -25,9 +27,15 @@ public class SignUpTest {
                 .ifPresent(WebElement::click);
         driver.findElements(By.xpath("//a[text()='  Sign Up']")).get(1).click();
         driver.findElement(By.name("firstname")).sendKeys("Kamil");
-        driver.findElement(By.name("lastname")).sendKeys("Hołody");
+        driver.findElement(By.name("lastname")).sendKeys(lastName);
         driver.findElement(By.name("phone")).sendKeys("511222323");
-        driver.findElement(By.name("email")).sendKeys("test@test.pl");
+        driver.findElement(By.name("email")).sendKeys("testkh2@test.pl");
+        driver.findElement(By.name("password")).sendKeys("Test123!");
+        driver.findElement(By.name("confirmpassword")).sendKeys("Test123!");
+        driver.findElement(By.xpath("//button[@type='submit' and text()=' Sign Up']")).click();
+        WebElement heading = driver.findElement(By.xpath("//h3[@class='RTL']"));
 
+        Assert.assertTrue(heading.getText().contains(lastName));
+        Assert.assertEquals(heading.getText(), "Hi, Kamil Testowy");
     }
 }
