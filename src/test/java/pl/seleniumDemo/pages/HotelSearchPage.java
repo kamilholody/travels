ac1636a850047a8da2c01a8c3831374769427e09
Kamil Holody <kamilholody@live.com>
2023-07-05 14:06:54 +0200

    264 - Fixing a bug

--- src/test/java/pl/seleniumDemo/pages/HotelSearchPage.java
+++ src/test/java/pl/seleniumDemo/pages/HotelSearchPage.java
@@ -1,4 +1,5 @@
 package pl.seleniumDemo.pages;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -11,9 +12,6 @@ public class HotelSearchPage {
 
         @FindBy(xpath = "//div[@id='select2-drop']//input" )
         private WebElement searchHotelIntput;
-
-        @FindBy(xpath = "//span[@class='select2-match' and text()='Dubai']" )
-        private WebElement hotelMatch;
 
         @FindBy(name = "checkin")
         private WebElement checkinInput;
@@ -33,9 +31,12 @@ public class HotelSearchPage {
         @FindBy(xpath = "//button[text()=' Search']")
         private WebElement searchButton;
 
+        private WebDriver driver;
+
         //Constructor
         public HotelSearchPage(WebDriver driver) {
             PageFactory.initElements(driver, this);
+            this.driver = driver;
         }
 
 
@@ -43,7 +44,8 @@ public class HotelSearchPage {
         public void setCity(String cityName) {
             searchHotelSpan.click();
             searchHotelIntput.sendKeys(cityName);
-            hotelMatch.click();
+            String xpath = String.format("//span[@class='select2-match' and text()='%s']", cityName);
+            driver.findElement(By.xpath(xpath)).click();
         }
 
         public void setDates(String checkin, String checkout) {
